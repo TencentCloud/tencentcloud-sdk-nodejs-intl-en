6615ce726df66b0d5c552090ac4e4e2355f4fe00
tencentcloudapi <tencentcloudapi@tencent.com>
2025-06-25 04:35:25 +0800

    release 3.0.1201

--- tencentcloud/common/sdk_version.js
+++ tencentcloud/common/sdk_version.js
@@ -1,2 +1,2 @@
-const sdkVersion = "3.0.1200";
+const sdkVersion = "3.0.1201";
 module.exports = sdkVersion
--- tencentcloud/iai/v20200303/iai_client.js
+++ tencentcloud/iai/v20200303/iai_client.js
@@ -392,8 +392,6 @@ The face quality information is mainly used to evaluate the quality of the input
 
 >     
 - Please use the signature algorithm v3 to calculate the signature in the common parameters, that is, set the `SignatureMethod` parameter to `TC3-HMAC-SHA256`.
-
-
      * @param {DetectFaceRequest} req
      * @param {function(string, DetectFaceResponse):void} cb
      * @public
